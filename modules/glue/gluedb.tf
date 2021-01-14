b359df2e646c0169457a60cda406c40a4f760f5b
fclaudiopalmeira <francisco.palmeira@cdhb.heath.nz>
2021-01-14 13:20:59 +1300

    Delete everything

--- modules/glue/gluedb.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
-  name = var.gluedbname
-  description = "Stores tables for the ecpp data"
-}
-
-## catalog_id - (Optional) ID of the Glue Catalog to create the database in. If omitted, this defaults to the AWS Account ID, and for ChestPain Pathways that is the desired Catalog ID.
-
-## Glue Database Name = _ecpp_glu
